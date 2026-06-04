fd0475fae5108adffa71e8e8231f0b28a58deec3
remabd <remiabdallah3@gmail.com>
2026-06-04 18:17:26 +0200

    feat-Utilisateur: Ajoute de la gestion d'erreur dans les requêtes

--- src/utilisateur/utilisateur.service.ts
+++ src/utilisateur/utilisateur.service.ts
@@ -1,6 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { Utilisateur, Prisma } from 'src/generated/prisma/client';
+import { Utilisateur, Prisma } from '../generated/prisma/client';
 import { UtilisateurWhereUniqueInput } from 'src/generated/prisma/models';
 import * as bcrypt from 'bcrypt';
 import 'dotenv/config';
@@ -12,10 +16,18 @@ export class UtilisateurService {
 
   async utilisateur(
     utilisateurWhereUniqueInput: UtilisateurWhereUniqueInput,
-  ): Promise<Utilisateur | null> {
-    return this.prisma.utilisateur.findUnique({
-      where: utilisateurWhereUniqueInput,
-    });
+  ): Promise<Utilisateur> {
+    try {
+      return await this.prisma.utilisateur.findUniqueOrThrow({
+        where: utilisateurWhereUniqueInput,
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError)
+        if (e.code === 'P2025') {
+          throw new NotFoundException();
+        }
+      throw e;
+    }
   }
 
   async utilisateurs(params: {
@@ -38,11 +50,16 @@ export class UtilisateurService {
   async creeUtilisateur(
     data: Prisma.UtilisateurCreateInput,
   ): Promise<Utilisateur> {
-    const hash = await bcrypt.hash(data.motDePasse, this.SALT);
-    data.motDePasse = hash;
-    return this.prisma.utilisateur.create({
-      data,
-    });
+    try {
+      const hash = await bcrypt.hash(data.motDePasse, this.SALT);
+      data.motDePasse = hash;
+      return await this.prisma.utilisateur.create({ data });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') throw new ConflictException();
+      }
+      throw e;
+    }
   }
 
   async modifieUtilisateur(params: {
@@ -50,17 +67,33 @@ export class UtilisateurService {
     data: Prisma.UtilisateurUpdateInput;
   }): Promise<Utilisateur> {
     const { where, data } = params;
-    return this.prisma.utilisateur.update({
-      data,
-      where,
-    });
+    try {
+      return await this.prisma.utilisateur.update({
+        data,
+        where,
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError)
+        if (e.code === 'P2025') {
+          throw new NotFoundException();
+        }
+      throw e;
+    }
   }
 
   async supprimeUtilisateur(
     where: Prisma.UtilisateurWhereUniqueInput,
   ): Promise<Utilisateur> {
-    return this.prisma.utilisateur.delete({
-      where,
-    });
+    try {
+      return await this.prisma.utilisateur.delete({
+        where,
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError)
+        if (e.code === 'P2025') {
+          throw new NotFoundException();
+        }
+      throw e;
+    }
   }
 }
