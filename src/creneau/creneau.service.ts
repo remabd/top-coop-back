3f19c449d3e3c5d3026337e6c96f91722c85021c
remabd <remiabdallah3@gmail.com>
2026-06-09 14:17:45 +0200

    feat-Mobile: Récupère détails du créneau pour consultation

--- src/creneau/creneau.service.ts
+++ src/creneau/creneau.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Creneau, Prisma } from '../generated/prisma/client';
 import { CreneauWhereUniqueInput } from '../generated/prisma/models';
 import { PrismaService } from '../prisma.service';
+import { JwtPayload } from 'src/auth/auth.guard';
 
 @Injectable()
 export class CreneauService {
@@ -38,5 +43,29 @@ export class CreneauService {
     return this.prisma.creneau.delete({
       where,
     });
+  }
+
+  async avecDetails(
+    where: Prisma.CreneauWhereUniqueInput,
+    utilisateur: JwtPayload | undefined,
+  ): Promise<Creneau> {
+    const creneau = await this.prisma.creneau.findUniqueOrThrow({
+      where,
+      include: { participations: true },
+    });
+    if (utilisateur) {
+      throw new UnauthorizedException('Authentification requise');
+    }
+    if (
+      creneau.participations.some(
+        (participation) => participation.utilisateurId === utilisateur!.sub,
+      )
+    ) {
+      return creneau;
+    } else {
+      throw new ForbiddenException(
+        "Vous n'êtes pas autorisé à consulter ces informations",
+      );
+    }
   }
 }
